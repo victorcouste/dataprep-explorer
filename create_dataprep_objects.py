fb069f766821ad9bcec4b522ddaf737f5ea786ac
Victor Coustenoble <victor.coustenoble@gmail.com>
2020-10-04 21:13:42 +0200

    v1

--- create_dataprep_objects.py
+++ create_dataprep_objects.py
@@ -3,44 +3,57 @@ import requests, json
 from datetime import datetime
 
 
-def get_recipe_url(bucket_name,file_path):
+# --- Token for Dataprep Authentication - Can be generated in Dataprep interface for project's owner
+DATAPREP_AUTH_TOKEN='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiZjE1MWI5ZGItMmEyMy00OWUzLTlhYmQtNWU4NmVjYjM1YWI2IiwiaWF0IjoxNTg3OTI3NjkxLCJhdWQiOiJ0cmlmYWN0YSIsImlzcyI6ImRhdGFwcmVwLWFwaS1hY2Nlc3MtdG9rZW5AdHJpZmFjdGEtZ2Nsb3VkLXByb2QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzdWIiOiJkYXRhcHJlcC1hcGktYWNjZXNzLXRva2VuQHRyaWZhY3RhLWdjbG91ZC1wcm9kLmlhbS5nc2VydmljZWFjY291bnQuY29tIn0.JcjPJFyhm13LFUjdnceToTU989kCNqowESm8q2Hrq680aMxQHdut3RFk2MVrFKdDIvkctXtwcgOEheSaHHjibKt1SsEsvmciesekltTQrs8Vdl5shXcD7ZcvtWJYarlKnKYaQhF-Bd04ReewsjQizLCwwW1cO2VmFF58iNQpyfNbcy4_xNNMrM00wjjiHazLGmsHR-ZKV7U57MkYpk0YidaUNirz_BEDYljfQumd4n4HvVfF9o5Lxd1lDU_gnaystPHuDn_B6TOzNHvxtUcorNn6B_tJSb9rkHsQVKrMhTDaB_o75Qy5C0C_gVtz0PBkL-2pEWMUakdPhC4lYNyBfw'
+# --- Dataprep folder ID to create new flows
+DATAPREP_FOLDERID=3128
 
-
-    host='https://api.clouddataprep.com'
-    datataprep_auth_token='eyJhbGciOiJSUzI1NiIsInR5cCI6IkpXVCJ9.eyJ0b2tlbklkIjoiZjE1MWI5ZGItMmEyMy00OWUzLTlhYmQtNWU4NmVjYjM1YWI2IiwiaWF0IjoxNTg3OTI3NjkxLCJhdWQiOiJ0cmlmYWN0YSIsImlzcyI6ImRhdGFwcmVwLWFwaS1hY2Nlc3MtdG9rZW5AdHJpZmFjdGEtZ2Nsb3VkLXByb2QuaWFtLmdzZXJ2aWNlYWNjb3VudC5jb20iLCJzdWIiOiJkYXRhcHJlcC1hcGktYWNjZXNzLXRva2VuQHRyaWZhY3RhLWdjbG91ZC1wcm9kLmlhbS5nc2VydmljZWFjY291bnQuY29tIn0.JcjPJFyhm13LFUjdnceToTU989kCNqowESm8q2Hrq680aMxQHdut3RFk2MVrFKdDIvkctXtwcgOEheSaHHjibKt1SsEsvmciesekltTQrs8Vdl5shXcD7ZcvtWJYarlKnKYaQhF-Bd04ReewsjQizLCwwW1cO2VmFF58iNQpyfNbcy4_xNNMrM00wjjiHazLGmsHR-ZKV7U57MkYpk0YidaUNirz_BEDYljfQumd4n4HvVfF9o5Lxd1lDU_gnaystPHuDn_B6TOzNHvxtUcorNn6B_tJSb9rkHsQVKrMhTDaB_o75Qy5C0C_gVtz0PBkL-2pEWMUakdPhC4lYNyBfw'
-    folderId=3128
-
-    file_uri="gs://"+bucket_name+"/"+file_path
+def get_dataprep_urls(file_uri):
 
     now = datetime.now() # current date and time
-    timestamp = now.strftime("%m/%d/%Y, %H:%M")
+    timestamp = now.strftime("%d/%m/%y %H:%M")
 
     #print("Today :", timestamp)
-
     #timestamp=`date +"%d/%m/%y %H:%M"`
     #echo $timestamp
 
-    flow_name="New Flow - "+timestamp
-    flow_description="New Flow description - "+timestamp
-    file_name="File - "+timestamp
+    flow_name="Flow - "+timestamp
+    flow_description="Flow description - "+timestamp
+    dataset_name="Dataset - "+timestamp
     recipe_name="Recipe - "+timestamp
 
-    # --------------------------------------------------------------------------------------------
-    # -------------- CREATE FLOW ---------------
-    # --------------------------------------------------------------------------------------------
+    new_flow_id = create_dataprep_flow(flow_name,flow_description)
+    new_dataset_id = create_dataprep_dataset(file_uri,dataset_name)
+    add_dataset_to_flow(new_flow_id,new_dataset_id)
+    new_recipe_id = add_recipe_to_dataset(new_flow_id,new_dataset_id,recipe_name)
+
+    recipe_url = 'https://clouddataprep.com/data/{}/{}'.format(new_flow_id,new_recipe_id)
+    dataset_url = 'https://clouddataprep.com/datasets/{}'.format(new_dataset_id)
+    flow_url = 'https://clouddataprep.com/flows/{}'.format(new_flow_id)
+
+    dataprep_urls={'recipe_url': recipe_url, 'dataset_url': dataset_url, 'flow_url': flow_url}
+
+    return dataprep_urls
+
+
+# --------------------------------------------------------------------------------------------
+# -------------- CREATE FLOW ---------------
+# --------------------------------------------------------------------------------------------
+
+def create_dataprep_flow(flow_name, flow_description):
 
     endpoint="/v4/flows"
 
     parameters = {
         "name": flow_name,
         "description": flow_description,
-        "folderId": folderId
+        "folderId": DATAPREP_FOLDERID
     }      
     #print('Parameter: {}'.format(parameters))
             
     resp = requests.post(
-        url=host+endpoint,
-        headers={"Content-Type":"application/json","Authorization": "Bearer "+datataprep_auth_token},
+        url="https://api.clouddataprep.com"+endpoint,
+        headers={"Content-Type":"application/json","Authorization": "Bearer "+DATAPREP_AUTH_TOKEN},
         data=json.dumps(parameters)
     )
     result=resp.json()
@@ -50,23 +63,25 @@ def get_recipe_url(bucket_name,file_path):
     #print('Result : {}'.format(result))
     #print('New Flow ID : {}'.format(new_flow_id))
 
-    # --------------------------------------------------------------------------------------------
-    # ------------------------------ CREATE DATASET ----------------------------------------------
-    # --------------------------------------------------------------------------------------------
+    return new_flow_id
+
+# --------------------------------------------------------------------------------------------
+# ------------------------------ CREATE DATASET ----------------------------------------------
+# --------------------------------------------------------------------------------------------
+
+def create_dataprep_dataset(file_uri,dataset_name):
 
     endpoint="/v4/importedDatasets"
-    detectStructure="false"
 
     parameters = {
         "uri": file_uri,
-        "name": file_name,
-        "detectStructure": detectStructure
+        "name": dataset_name
     }      
     #print('Parameter: {}'.format(parameters))
             
     resp = requests.post(
-        url=host+endpoint,
-        headers={"Content-Type":"application/json","Authorization": "Bearer "+datataprep_auth_token},
+        url="https://api.clouddataprep.com"+endpoint,
+        headers={"Content-Type":"application/json","Authorization": "Bearer "+DATAPREP_AUTH_TOKEN},
         data=json.dumps(parameters)
     )
     result=resp.json()
@@ -76,20 +91,25 @@ def get_recipe_url(bucket_name,file_path):
     #print('Result : {}'.format(result))
     #print('New Dataset ID : {}'.format(new_dataset_id))
 
-    # --------------------------------------------------------------------------------------------
-    # -------------- ADD DATASET TO FLOW ---------------
-    # --------------------------------------------------------------------------------------------
+    return new_dataset_id
 
-    endpoint="/v4/importedDatasets/"+str(new_dataset_id)+"/addToFlow"
+# --------------------------------------------------------------------------------------------
+# -------------- ADD DATASET TO FLOW ---------------
+# --------------------------------------------------------------------------------------------
+
+def add_dataset_to_flow(flow_id,dataset_id):
+
+
+    endpoint="/v4/importedDatasets/"+str(dataset_id)+"/addToFlow"
 
     parameters = {
-        "flow": {"id": new_flow_id}
+        "flow": {"id": flow_id}
     }      
     #print('Parameter: {}'.format(parameters))
             
     resp = requests.post(
-        url=host+endpoint,
-        headers={"Content-Type":"application/json","Authorization": "Bearer "+datataprep_auth_token},
+        url="https://api.clouddataprep.com"+endpoint,
+        headers={"Content-Type":"application/json","Authorization": "Bearer "+DATAPREP_AUTH_TOKEN},
         data=json.dumps(parameters)
     )
     result=resp.json()
@@ -97,22 +117,27 @@ def get_recipe_url(bucket_name,file_path):
     #print('Status Code : {}'.format(resp.status_code))
     #print('Result : {}'.format(result))
 
-    # --------------------------------------------------------------------------------------------
-    # -------------- ADD RECIPE TO DATASET ---------------
-    # --------------------------------------------------------------------------------------------
+    return
+
+# --------------------------------------------------------------------------------------------
+# -------------- ADD RECIPE TO DATASET ---------------
+# --------------------------------------------------------------------------------------------
+
+def add_recipe_to_dataset(flow_id,dataset_id,recipe_name):
+
 
     endpoint="/v4/wrangledDatasets"
 
     parameters = {
-        "importedDataset":{"id": new_dataset_id},
-        "flow": {"id": new_flow_id},
+        "importedDataset":{"id": dataset_id},
+        "flow": {"id": flow_id},
         "name": recipe_name
     }      
     #print('Parameter: {}'.format(parameters))
             
     resp = requests.post(
-        url=host+endpoint,
-        headers={"Content-Type":"application/json","Authorization": "Bearer "+datataprep_auth_token},
+        url="https://api.clouddataprep.com"+endpoint,
+        headers={"Content-Type":"application/json","Authorization": "Bearer "+DATAPREP_AUTH_TOKEN},
         data=json.dumps(parameters)
     )
 
@@ -123,13 +148,4 @@ def get_recipe_url(bucket_name,file_path):
     #print('Result : {}'.format(result))
     #print('New Recipe ID : {}'.format(new_recipe_id))
 
-
-# --------------------------------------------------------------------------------------------
-# --------------------------------------------------------------------------------------------
-
-#print('Flow URL : https://clouddataprep.com/flows/{}'.format(new_flow_id))
-
-#print('Dataset URL : https://clouddataprep.com/data/{}/{}'.format(new_flow_id,new_recipe_id))
-    recipe_url = 'https://clouddataprep.com/data/{}/{}'.format(new_flow_id,new_recipe_id)
-
-    return recipe_url
+    return new_recipe_id
--- datataprep-explorer.py
+++ datataprep-explorer.py
@@ -8,11 +8,21 @@ app = Flask(__name__)
 def home():
 
     recipe_url=''
+    dataset_url=''
+    flow_url=''
+    file_uri=''
 
     if request.method == 'POST':
 
         bucket=request.form['bucket']
         filepath=request.form['filepath']
-        recipe_url=create_dataprep_objects.get_recipe_url(bucket,filepath)
 
-    return render_template('home.html',recipe_url=recipe_url)
+        file_uri="gs://"+bucket+"/"+filepath
+        
+        dataprep_urls=create_dataprep_objects.get_dataprep_urls(file_uri)
+
+        recipe_url=dataprep_urls['recipe_url']
+        dataset_url=dataprep_urls['dataset_url']
+        flow_url=dataprep_urls['flow_url']
+
+    return render_template('home.html',file_uri=file_uri,recipe_url=recipe_url,dataset_url=dataset_url,flow_url=flow_url)
